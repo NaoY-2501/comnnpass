3611eccb02c243e3870838c33e6a767cde24b0cf
NaoY-2501 <solaris2501.fe2@gmail.com>
2017-04-07 00:07:18 +0900

    Add STATIC_ROOT to settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
