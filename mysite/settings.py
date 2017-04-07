f0137186befd79849501f4c80675f59084035d66
NaoY-2501 <solaris2501.fe2@gmail.com>
2017-04-07 22:18:12 +0900

    Add logo link

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,7 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware'
 ]
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
 ROOT_URLCONF = 'mysite.urls'
 
